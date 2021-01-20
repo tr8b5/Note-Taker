e4656960a18f7393199f7e96efef4e9109d24acd
William Miller <williamhmiller12@gmail.com>
2021-01-19 22:34:27 -0500

    I fixed heroku server issue! I think

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var express = require("express");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = io.listen(process.env.PORT) || 3000;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
